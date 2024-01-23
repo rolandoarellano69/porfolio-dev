cc5a2e891529216022a66e48526454007e5bdb6c
Rolando Arellano <rolandoarellano9869@hotmail.com>
2024-01-23 13:12:05 -0800

    Fix typo in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapater: 'vercel'({
+  adapter: vercel({
     webAnalytics:{
       enabled: true,
     },
