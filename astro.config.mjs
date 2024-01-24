3cccdb3688768c73a051a704c67e38967a594651
Rolando Arellano <rolandoarellano9869@hotmail.com>
2024-01-24 08:26:17 -0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,8 @@
-import { defineConfig } from "astro/config";
+import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/serverless";
+
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
-  adapter: vercel({
-    webAnalytics: {
-      enabled: true,
-    },
-  }),
-  integrations: [tailwind()],
+  integrations: [tailwind()]
 });
